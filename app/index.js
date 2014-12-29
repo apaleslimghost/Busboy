ec31825cf3dc13bc83a61097045ea8dc3732076c
Matt Brennan <mattyb1000@gmail.com>
2014-12-29 14:52:26 +0000

    styles, testing mui

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 var React = require('react');
+var mui   = require('material-ui');
 
 var Busboy = React.createClass({
   render() {
-    return <h1>It probably works!</h1>;
+    return <mui.Toolbar/>;
   }
 });
 
 React.render(<Busboy/>, document.body);
+require('./index.less');
