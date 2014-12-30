29a1c9cd1753235757dfcbccd8ae20aaa9f7f352
Matt Brennan <mattyb1000@gmail.com>
2014-12-30 21:33:02 +0000

    initial data-getting

--- app/index.js
+++ app/index.js
@@ -1,8 +1,28 @@
 var React = require('react');
+var {BaconMixin} = require('react-bacon');
+var busboy = require('tfl-busboy');
+var _ = require('underscore');
 
 var Busboy = React.createClass({
+  mixins: [BaconMixin],
+
+  getInitialState() {
+    return {
+      stops: {}
+    };
+  },
+
+  componentWillMount() {
+    this.plug(busboy.around({
+      lat: 51.371422, lng: -0.227344
+    }, 100), 'stops');
+  },
+
   render() {
-    return <h1>Busboy</h1>;
+    return <div>
+      <h1>Busboy</h1>
+      <ul>{_.map(this.state.stops, (stop) => <li>{stop.stopId}</li>)}</ul>
+    </div>;
   }
 });
 
