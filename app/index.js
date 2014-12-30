b59cfabb9771bc08ed3d7773087f543a2577970c
Matt Brennan <mattyb1000@gmail.com>
2014-12-30 21:37:15 +0000

    loading indicator

--- app/index.js
+++ app/index.js
@@ -8,23 +8,24 @@ var Busboy = React.createClass({
 
   getInitialState() {
     return {
-      stops: {}
+      meta: {}
     };
   },
 
   componentWillMount() {
     this.plug(busboy.around({
       lat: 51.371422, lng: -0.227344
-    }, 100), 'stops');
+    }, 100));
   },
 
   stops() {
-    return _.reject(this.state.stops, (v, k) => k === 'meta');
+    return _.reject(this.state, (v, k) => k === 'meta');
   },
 
   render() {
     return <div>
       <h1>Busboy</h1>
+      {this.state.meta.loading && 'loading'}
       <ul>{_.map(this.stops(), (stop) => <li>{stop.stopId}</li>)}</ul>
     </div>;
   }
