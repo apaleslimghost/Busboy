6d0b53555aba5d71e739ef7bb24af46ffb227b99
Matt Brennan <mattyb1000@gmail.com>
2014-12-30 18:34:33 +0000

    convert to sass, nix material-ui

--- app/index.js
+++ app/index.js
@@ -1,11 +1,10 @@
 var React = require('react');
-var mui   = require('material-ui');
 
 var Busboy = React.createClass({
   render() {
-    return <h1>Frob</h1>;
+    return <h1>Busboy</h1>;
   }
 });
 
 React.render(<Busboy/>, document.body);
-require('./index.less');
+require('./index.scss');
