b4f4766b00c52db03bec5b1507842a9c4a5c8add
Matt Brennan <mattyb1000@gmail.com>
2016-11-23 23:36:27 +0000

    native font stack. miss you aeileron :(

--- offline/resources.js
+++ offline/resources.js
@@ -3,15 +3,7 @@ exports.resources = [
 	'/index.html',
 	'/main.css',
 	'/main.js',
-	'/images/logo.png',
-	'/fonts/aileron-bold-webfont.eot',
-	'/fonts/aileron-bold-webfont.ttf',
-	'/fonts/aileron-bold-webfont.woff',
-	'/fonts/aileron-bold-webfont.woff2',
-	'/fonts/aileron-regular-webfont.eot',
-	'/fonts/aileron-regular-webfont.ttf',
-	'/fonts/aileron-regular-webfont.woff',
-	'/fonts/aileron-regular-webfont.woff2'
+	'/images/logo.png'
 ];
 
 exports.version = 3;
