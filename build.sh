2992aee5400d8ec88a6fcc0f3bd46f5c34346c15
Matt Brennan <mattyb1000@gmail.com>
2016-11-24 11:51:32 +0000

    minify

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x -e
+
+export NODE_ENV=production
+
+node-sass styles/main.scss > public/main.css
+browserify app/index.jsx | babili > public/main.js
+browserify offline/service-worker.js | babili > public/sw.js
+node server/manifest > public/manifest.appcache
+
+cp -r public server/public
