7a9dc86b564645e69ed315bb902c678c4723f183
Matt Brennan <mattyb1000@gmail.com>
2016-11-24 11:59:01 +0000

    gzip

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ browserify app/index.jsx | babili > public/main.js
 browserify offline/service-worker.js | babili > public/sw.js
 node server/manifest > public/manifest.appcache
 
+rm -rf server/public
 cp -r public server/public
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const compression = require('compression');
+
 const app = require('./');
 const port = process.env.PORT || 3000;
 
+app.use(compression());
 app.use('/', express.static('public'));
 app.listen(port, () => console.log(`listening on ${port}`));
